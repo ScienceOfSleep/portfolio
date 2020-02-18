707e934491d18f5301416b3e025baab7c5aae449
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2020-02-18 00:31:01 -0700

    Post Template Fix

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,34 +5,50 @@ import SEO from "../components/seo";
 import Logo from "../components/logo";
 import Img from "gatsby-image";
 import NavBar from "../components/navbar";
+import MainGrid from "../components/maingrid";
+import TitleSection from "../components/titlesection";
+import SkinnyHero from "../components/skinnyhero";
+import {css} from "@emotion/core";
 
 export default ({ data }) => {
     return (
         <Layout>
             {data.allWordpressPost.edges.map(({ node }) =>
-                <div className={'mainGrid'} key={node.id}>
+                <MainGrid key={node.id}>
                     <SEO title={node.title} description={node.content} />
-                    <div className={'navDiv'}>
-                        <NavBar/>
-                    </div>
-                    <h1 className={'subtitle'}>
-                        {node.title}
-                    </h1>
-                    <h2 className={'title'}>
-                        By Josh Steele
-                    </h2>
-                    <div className={'hero'}>
-                        <Img
-                            fluid={data.desktopImage.childImageSharp.fluid}
-                            alt=""
-                        />
-                    </div>
-                    <div className={'longContent'} dangerouslySetInnerHTML={{ __html: node.content }} />
-                    <div className={'logo'}>
-                        <Logo/>
-                    </div>
-                    <div className={'darkBar'}></div>
-                </div>
+
+                    <NavBar/>
+
+                    <Logo/>
+
+                    <TitleSection>
+                        <h1>{node.title}</h1>
+                        <h2>By Josh Steele</h2>
+                    </TitleSection>
+
+                    <SkinnyHero
+                        fluid={data.desktopImage.childImageSharp.fluid}
+                        alt="Random Unsplash image for flavor"
+                    />
+
+                    <section
+                            css={css`
+                              grid-area: mainCopy;
+                              padding-right: 1rem;
+                              padding-left: 1rem;
+                              align-self: center;
+                              h4{
+                              padding-top: .5rem;
+                              }
+                              @media only screen and (min-width: 64.063em) {
+                              padding-right: 4rem;
+                              padding-left: 0;
+                              }
+                            `}
+                            dangerouslySetInnerHTML={{ __html: node.content }}
+                    />
+
+                </MainGrid>
             )}
         </Layout>
     )
